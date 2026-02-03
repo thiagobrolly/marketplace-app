27d9b08ee2332b9a14c712a200f9961dde647e44
Thiago Brolly <thiagoex377@gmail.com>
2026-02-03 15:02:11 -0300

    feat: configurando nativewind

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Login() {
   return (
     <View>
-      <Text>Login Page</Text>
+      <Text className='text-6xl text-purple-base'>Login Page</Text>
     </View>
   );
 }
