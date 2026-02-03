b90565bcbd3fd205f15a8f4111a68064926331c5
Thiago Brolly <thiagoex377@gmail.com>
2026-02-03 16:34:33 -0300

    feat: configurando react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from 'expo-router';
-import "../styles/global.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '../styles/global.css';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
